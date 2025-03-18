9317caed735142535d316054576c05625d6fa072
BaselTurkman <Basel.Turkman123@gmail.com>
2025-03-18 14:52:19 +0200

    Handle Errors in PostRequest Functionality

--- js/postRequest.js
+++ js/postRequest.js
@@ -1,8 +1,18 @@
 export async function postRequest(data) {
-    return fetch('https://dummyjson.com/todos/add', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(data),
-    });
+    try {
+        const response = await fetch('https://dummyjson.com/todos/add', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        });
 
+        if (!response.ok) {
+            throw new Error(`Failed to create TODO, Status: ${response.status}`);
+        }
+
+        return await response.json();
+    } catch (error) {
+        console.error("Error posting todos:", error.message);
+        return { success: false, message: error.message };
+    }
 }
